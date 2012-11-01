9ca12ff440df75ce9386cc01b40665f8c5ac7248
Elise Huard <elise@jabberwocky.eu>
2012-11-01 17:40:37 +0000

    simpler code by removing namespacing

--- js/spec/identity-monad-spec.js
+++ js/spec/identity-monad-spec.js
@@ -1,63 +1,66 @@
 var vows   = require('vows'),
     assert = require('assert');
-var monads = require('../identity-monad.js');
+var monads = require('../identity-monad.js'),
+    wrap   = monads.identityMonad.wrap,
+    bind   = monads.identityMonad.bind,
+    lift   = monads.identityMonad.lift;
 
 vows.describe('Identity Monad').addBatch({
   'wrap generates a monadic value closing over the value': {
-    topic: function() { return monads.identityMonad.wrap('candy') },
+    topic: function() { return wrap('candy') },
     'when calling the monadic value we get the value back': function(topic) {
       assert.equal (topic(), 'candy');
     }
   },
   'bind binds a monadic value to a monad-producing function': {
-    topic: function() { return monads.identityMonad.bind },
+    topic: function() { return bind },
     'when calling the resulting monadic value we get the result of value + function' : function(topic) {
       var more = function(value) {
-          return monads.identityMonad.wrap("more " + value);
+          return wrap("more " + value);
       }
-      var mv = monads.identityMonad.wrap('candy');
+      var mv = wrap('candy');
       var bound = topic(mv, more);
       assert.equal (bound(), 'more candy');
     }
   },
   'lift makes a non-monadic function into a monad-producing and consuming function': {
-    topic: function() { return monads.identityMonad.lift },
+    topic: function() { return lift },
     'when feeding it a monadic value, you get a monadic value from the result': function(topic) {
       var normalMore = function(value) {
         return "more " + value;
       }
       var mMore = topic(normalMore);
-      var mv = monads.identityMonad.wrap('candy');
+      var mv = wrap('candy');
       assert.equal(mMore(mv)(), 'more candy');  
     }  
   },
   'first monadic law': {
     'left unit: wrap acts as neutral element of bind': function() {
       var more = function(value) {
-          return monads.identityMonad.wrap("more " + value);
+          return wrap("more " + value);
       }
-      assert.equal(monads.identityMonad.bind(monads.identityMonad.wrap("candy"), more)(),
+      assert.equal(bind(wrap("candy"), more)(),
                    more("candy")());
     }
   },
   'second monadic law': {
     'right unit: wrap acts as neutral element of bind': function() {
-      assert.equal(monads.identityMonad.bind(monads.identityMonad.wrap("candy"), monads.identityMonad.wrap)(),
-                   monads.identityMonad.wrap("candy")());
+      assert.equal(bind(wrap("candy"), wrap)(),
+                   wrap("candy")());
     }
   },
   'third monadic law': {
     'associative: binding two functions in succession is equal to binding a combination of the two functions': function() {
       var f = function(value) {
-          return monads.identityMonad.wrap("more " + value);
+          return wrap("much " + value);
       }
       var g = function(value) {
-          return monads.identityMonad.wrap("much " + value);
+          return wrap("more " + value);
       }
-      var mv = monads.identityMonad.wrap("candy");
+      var mv = wrap("candy");
 
-      assert.equal(monads.identityMonad.bind(monads.identityMonad.bind(mv,f),g)(),
-                   monads.identityMonad.bind(mv,function(x) { return monads.identityMonad.bind(f(x),g) })());
+      assert.equal(bind(bind(mv,f),g)(),
+                   bind(mv,function(x) { return bind(f(x),g) })());
     }
   }
 }).export(module);

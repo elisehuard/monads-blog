2ba2dd87b90e539505841961ba56ba0206b943a4
Elise Huard <elise@jabberwocky.eu>
2012-11-02 17:51:48 +0000

    add chaining functions to state monads in js

--- js/spec/state-monad-spec.js
+++ js/spec/state-monad-spec.js
@@ -28,14 +28,34 @@ vows.describe('State Monad').addBatch({
     'can also bind to a function that adds on the state' : function() {
       var addSugar = function(drink) {
         return function(sugars) {
-          return { drink: drink, sugars: sugars - 1 }
+          return { drink: drink, sugars: sugars + 1 }
         }
       }
       var mv = wrap('coffee');
       var bound = bind(mv, addSugar);
       var realized = bound(3);
       assert.equal (realized.drink, 'coffee');
-      assert.equal (realized.sugars, 2);
+      assert.equal (realized.sugars, 4);
     }
   },
+  'chaining functions for fun and profit': function() {
+    var more = function(value) {
+        return wrap("more " + value);
+    }
+    var addSugar = function(drink) {
+      return function(sugars) {
+        return { drink: drink, sugars: sugars + 1 }
+      }
+    }
+    var mMore = function(mv) {
+      return bind(mv, more);
+    }
+    var mAddSugar = function(mv) {
+      return bind(mv, addSugar);
+    }
+
+    var realized = mMore(mAddSugar(mMore(mAddSugar(wrap("coffee")))))(0);
+    assert.equal (realized.drink, 'more more coffee');
+    assert.equal (realized.sugars, 2);
+  }
 }).export(module);
